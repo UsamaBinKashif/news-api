f8a03332d1f6253eb3266a6142c79f1e0c4a4b51
Usama Ahmed <usamabinkashif.98@gmail.com>
2022-04-03 01:08:42 +0500

    commit

--- scripts/app.js
+++ scripts/app.js
@@ -21,6 +21,7 @@ XHR.onload = function () {
             <img class="lg:h-48 md:h-30 w-full object-contain object-cover " src="${element.image}"
               alt="blog">
             <div class="p-6">
+            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">${element.source.name}</h2>
               <h1 class="title-font text-lg font-medium text-gray-50 mb-3">${element.title}</h1>
               <p class="leading-relaxed mb-3 text-gray-400">${element.description}</p>
               <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" href="${element.url}" target="_blank" >See More</a>
