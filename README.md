a39c96d2d8bddd748760d0c4fca38b4acf90640b
Usama Ahmed <usamabinkashif.98@gmail.com>
2022-04-02 16:28:10 +0500

    first-commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<a href="https://newsapi.org/" target="_blank" style="background-color:blue; text-decoration:none; color:white;cursor:pointer;  border:2px solid white; font-size:1.7rem; padding:3px; text-align:center;">NEWS <span style="color:blue; background-color:white; ">API</span></a>
+
+## Search worldwide news with code.
+
+### Locate articles and breaking news headlines from news sources and blogs across the web with this JSON API.
---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+let apiKey = "5f2bfb11fc40488ca69c0251790b3fb6";
+let source = "bbc-news";
+let main = document.querySelector(".main");
+
+let XHR = new XMLHttpRequest();
+XHR.open(
+  "GET",
+  `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`,
+  true
+);
+
+XHR.onload = function () {
+  if (XHR.status === 200) {
+    let news = JSON.parse(this.responseText);
+    let articles = news.articles;
+
+    articles.forEach((element) => {
+      let card = `
+      <div class="p-4 md:w-1/3">
+          <div class="h-full border-2 border-blue-600 border-opacity-60 rounded-lg overflow-hidden">
+            <img class="lg:h-48 md:h-25 w-full object-fit " src="${element.urlToImage}"
+              alt="blog">
+            <div class="p-6">
+              <h1 class="title-font text-lg font-medium text-gray-50 mb-3">${element.title}</h1>
+              <p class="leading-relaxed mb-3 text-gray-400">${element.description}</p>
+              <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0" href="${element.url}" target="_blank" >See More</a>
+            </div>
+          </div>
+        </div>`;
+      main.innerHTML += card;
+    });
+  } else if (XHR.status === 404) {
+    document.write("NOT FOUND");
+  }
+};
+XHR.send();
